88a6b7f853beb4894fb8c5cf5444063b6f507256
Vienio99 <kyve96@gmail.com>
2021-04-11 20:10:40 +0200

    added buttons

---
+++ index.js
@@ -0,0 +1,23 @@
+// Basic math functions
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+// Operator
+
+function operator(operator, a, b) {
+    return operator(a, b);
+}
